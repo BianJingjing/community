3c526dd7fa25ee076b0e01669346c56b8e01a532
BianJingjing <201710311048@stu.shmtu.edu.cn>
2020-07-18 13:08:39 +0800

    发布问题和首页列表展示

--- src/main/java/com/xiaobian/community/controller/IndexController.java
+++ src/main/java/com/xiaobian/community/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.xiaobian.community.controller;
 
+import com.xiaobian.community.dto.QuestionDTO;
+import com.xiaobian.community.mapper.QuestionMapper;
 import com.xiaobian.community.mapper.UserMapper;
+import com.xiaobian.community.model.Question;
 import com.xiaobian.community.model.User;
+import com.xiaobian.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,26 +14,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class IndexController {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private QuestionService questionService;
+
     @GetMapping("/")
-    public String index(HttpServletRequest httpServletRequest){
+    public String index(HttpServletRequest httpServletRequest,
+                        Model model){
+        //检验登录状态
         Cookie[] cookies = httpServletRequest.getCookies();
-        for (Cookie cookie:cookies) {
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if(user !=null){
-                    httpServletRequest.getSession().setAttribute("user",user);
+        if (cookies != null && cookies.length !=0){
+            for (Cookie cookie:cookies) {
+                if(cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if(user !=null){
+                        httpServletRequest.getSession().setAttribute("user",user);
+                    }
+                    break;
                 }
-                break;
             }
         }
-
+        List<QuestionDTO> questionList = questionService.list();
+        model.addAttribute("questions",questionList);
         return "index";
     }
 }
---
+++ src/main/java/com/xiaobian/community/service/QuestionService.java
@@ -0,0 +1,37 @@
+package com.xiaobian.community.service;
+
+import com.xiaobian.community.dto.QuestionDTO;
+import com.xiaobian.community.mapper.QuestionMapper;
+import com.xiaobian.community.mapper.UserMapper;
+import com.xiaobian.community.model.Question;
+import com.xiaobian.community.model.User;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    public List<QuestionDTO> list() {
+        List<Question> questions = questionMapper.list();
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+        for (Question question : questions){
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        return questionDTOList;
+    }
+}
